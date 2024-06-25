4c712e3fd445494836e25bdcbc8d1b1aed4bf6b8
daniel beyene <dazimore@gmail.com>
2024-06-25 13:04:14 +0300

    Mapping A Python Class to a Database Table : Code-Along

--- lib/department.py
+++ lib/department.py
@@ -1,12 +1,93 @@
 from __init__ import CURSOR, CONN
 
 
-class Department:
+class Department: 
 
-    def __init__(self, name, location, id=None):
-        self.id = id
+   
+    from __init__ import CURSOR, CONN
+
+
+class Department: 
+
+    def __init__(self, name, location):
+        self.id = None
         self.name = name
         self.location = location
 
-    def __repr__(self):
+    def __str__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+    
+
+    # add new ORM methods after existing methods ....
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of Department instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persists Department instances """
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+
+
+    # existing methods ...
+
+    def save(self):
+        """ Insert a new row with the name and location values of the current Department instance.
+        Update object id attribute using the primary key value of new row.
+        """
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+    
+
+    # ... existing methods
+
+    @classmethod
+    def create(cls, name, location):
+        """ Initialize a new Department instance and save the object to the database """
+        department = cls(name, location)
+        department.save()
+        return department
+    
+
+
+
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
